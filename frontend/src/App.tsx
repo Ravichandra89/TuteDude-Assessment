8e10bf7c5cdc57dba7280c02b3c9a9c1bb9e2827
Ravi chandra <githubravi01@gmail.com>
2025-09-17 19:27:29 +0530

    feat: setup frontend dependencies (react, vite, tailwind, axios etc..)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-6xl">Hello Welcome to Interview-Buddy</p>
+    </div>
+  );
+};
+
+export default App;
